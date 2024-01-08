452bacd2528e3862b810a1f4baafb039230137fb
yitingwu31 <animalfamily31@gmail.com>
2024-01-08 10:56:32 +0800

    update cv

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Yi-Ting Wu';
+export const SITE_DESCRIPTION = 'Yi-Ting Wu is a MSEE student from Stanford, aiming at becoming an impactful software engineer.';
